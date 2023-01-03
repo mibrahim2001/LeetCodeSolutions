5b1bd3c4896210f8ed39e99bef023032e1f49c6f
Ibrahim <mibrahim37612@gmail.com>
2023-01-04 00:03:23 +0500

    Added problem 14

---
+++ Solutions/14_ Longest_Common_Prefix/LongestCommonPrefix.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        // Sort the list of strings to make the search for the common prefix more
+        // efficient
+        Arrays.sort(strs);
+        // Get the first and last string in the sorted list
+        String firstStr = strs[0];
+        String lastStr = strs[strs.length - 1];
+
+        // Initialize a variable to store the common prefix
+        String commonPrefix = "";
+        // Iterate through the characters in the first and last string
+        for (int i = 0; i < Math.min(firstStr.length(), lastStr.length()); i++) {
+            // If the characters are the same, add it to the common prefix
+            if (firstStr.charAt(i) == lastStr.charAt(i)) {
+                commonPrefix += firstStr.charAt(i);
+            }
+            // If the characters are different, return the common prefix
+            else {
+                return commonPrefix;
+            }
+        }
+        // If the common prefix is an empty string, return an empty string
+        if (commonPrefix.isEmpty()) {
+            return "";
+        }
+        // Return the common prefix
+        return commonPrefix;
+    }
+}
--- Solutions/520_Detect_Capital/DetectCapital.java
+++ Solutions/520_Detect_Capital/DetectCapital.java
@@ -1,11 +1,8 @@
 
-
 public class DetectCapital {
     public static boolean detectCapitalUse(String word) {
         String regex = "^([A-Z]+|[a-z]+|[A-Z][a-z]*)$";
         return (word.matches(regex));
-
-
     }
 
     public static void main(String[] args) {
